44c627d54544c56bfd803c038fdd6bedf1204c87
RazSoft <razgroupgame@gmail.com>
2025-09-15 14:20:52 +0530

    Add link to the product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function ProductCard({ name, image }) {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center rounded-xl border-1 hover:scale-105 transition-all duration-200 ease-in">
-      <div className="overflow-hidden  rounded-2xl">
-        <img src={image} alt={"image of " + name} />
+    <Link to={"/product-details/2"}>
+      <div className="w-full h-full flex flex-col items-center justify-center rounded-xl border-1 hover:scale-105 transition-all duration-200 ease-in">
+        <div className="overflow-hidden  rounded-2xl">
+          <img src={image} alt={"image of " + name} />
+        </div>
+        <div>
+          <span>Product Name</span>
+        </div>
       </div>
-      <div>
-        <span>Product Name</span>
-      </div>
-    </div>
+    </Link>
   );
 }
